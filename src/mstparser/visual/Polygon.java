4337c1882a6626751992569c715783b089e8179a
delliott <delliott@links>
2013-01-29 18:43:03 +0000

    Attempting to correctly integrate visual features into the parsing model and experiencing difficulties with calculating the angle between two centroids.
    Also updated the unit testing example because it lagged behind.

--- src/mstparser/visual/Polygon.java
+++ src/mstparser/visual/Polygon.java
@@ -2,6 +2,7 @@ package mstparser.visual;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.awt.geom.Point2D;
 
 /**
  * This class represents an annotated image region as represented in a LabelMe
@@ -15,9 +16,47 @@ import java.awt.Point;
 
 public class Polygon {
 
-	public Point2Df centroid;
+	public Point2D centroid;
 	public double convexHullArea;
-	public SpatialRelation[] spatialRelations;
+	public SpatialRelation.Relations[] spatialRelations;
 	public Color averageRGB;
 	public ImageQuadrant imageQuadrant;
+	public String label;
+	public Point2D[] points;
+	
+	public Polygon(String polygonLabel)
+	{
+	    this.label = polygonLabel;
+	}
+	
+	public void setPoints(Point2D[] parsedPoints)
+	{
+	    this.points = parsedPoints;
+	    this.centroid = Point2Df.centerOfMass(this.points);
+	    this.convexHullArea = Point2Df.area(this.points);
+	}
+	
+	public String toString()
+	{
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("Object " + this.label + "\n");
+	    sb.append("---\n");
+	    sb.append("Centroid: " + this.centroid + "\n");
+	    sb.append("---\n");
+	    sb.append("Area: " + this.convexHullArea + "\n");
+	    sb.append("Points\n");
+	    for (Point2D p: points)
+	    {
+	        sb.append("("+p.getX()+","+p.getY()+")\n");
+	    }
+	    sb.append("---\n");
+	    sb.append("Spatial Relations\n");
+	    for (SpatialRelation.Relations r: spatialRelations)
+	    {
+	        sb.append(r + "\n");
+	    }
+	    sb.append("\n");
+	    return sb.toString();	         
+	}
+	
 }
--- src/mstparser/visual/SpatialRelation.java
+++ src/mstparser/visual/SpatialRelation.java
@@ -9,10 +9,53 @@ package mstparser.visual;
  */
 public class SpatialRelation {
 	
-	public enum Relations {ABOVE, BEHIND, BELOW, BESIDE, INFRONT, OPPOSITE, SURROUNDS, ROOT};
+	public enum Relations {ABOVE, BEHIND, BELOW, BESIDE, INFRONT, OPPOSITE, SURROUNDS, ROOT, NONE};
 	
 	public static Relations GetSpatialRelationship(Polygon p1, Polygon p2)
 	{
-		return Relations.ROOT;
+	    if (p1.equals(p2))
+        {
+            return Relations.NONE;
+        }
+	    double angle = getAngle(p1, p2);
+	    System.out.println(p1.label + "-" + p2.label + ":" + angle);
+	    double mAngle = angle - 0.00001;	    
+	    if (mAngle > 315.0 && mAngle < 360.0 || mAngle > 1.0 && mAngle < 45.0)
+	    {
+	        return Relations.BELOW;
+	    }
+	    else if (mAngle > 45.0 && mAngle < 135.0 || mAngle > 225.0 && mAngle < 315.0)
+	    {
+	        return Relations.BESIDE;
+	    }
+	    else if (mAngle > 135.0 && mAngle < 225.0)
+	    {
+	        return Relations.ABOVE;
+	    }
+	    return Relations.NONE;
 	}
+	
+	   /** Fetches angle relative to this polygon centroid
+     *  where 12 O'Clock is 0 and 6 O'Clock is 180 degrees
+     * 
+     * @param 
+     * @return angle in degress from 0-360.
+     */
+    public static double getAngle(Polygon one, Polygon two)
+    {
+        double dx = -(two.centroid.getX() - one.centroid.getX());
+        double dy = -(two.centroid.getY() - one.centroid.getY());
+
+        System.out.println(dy + " " + dx);
+        
+        double inRads = Math.atan2(dy,dx);
+
+        // We need to map to coord system when 0 degree is at 3 O'clock, 270 at 12 O'clock
+        /*if (inRads < 0)
+        {
+            inRads += 2*Math.PI;
+        }*/
+
+        return Math.toDegrees(inRads);
+    }   
 }
