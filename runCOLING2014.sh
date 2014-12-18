b6ea028c36499211846d07704c96f96e74d03ca4
Desmond Elliott <des.elliott@gmail.com>
2014-12-18 10:10:55 +0100

    Fixing directory references

--- runCOLING2014.sh
+++ runCOLING2014.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Train the VDR Parsing Model. No visual features for the EMNLP paper.
-python trainVDRParser.py -p ../../data/vlt/emnlp2013/ -m mst -k 5 -d non-proj -s 10 -f true -i true
+python trainVDRParser.py -p data/vlt/emnlp2013/ -m mst -k 5 -d non-proj -s 10 -f true -i true
 
 # Predict the VDRs for the test splits.
-python testVDRParser.py -p ../../data/vlt/emnlp2013/ -m mst -k 5 -d non-proj -s 10 -f true -i true
+python testVDRParser.py -p data/vlt/emnlp2013/ -m mst -k 5 -d non-proj -s 10 -f true -i true
 
 # Evaluate the performance of the EMNLP VDR Parsing Model
-python evaluateVDRParser.py -p ../../data/vlt/emnlp2013/ -s 10 -f true
+python evaluateVDRParser.py -p data/vlt/emnlp2013/ -s 10 -f true
